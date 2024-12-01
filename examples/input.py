5cb3e3f33ea750f3f8f80a55a4974fd5b1fbe4c9
Christopher <thisbuglem.e@gmail.com>
2024-12-01 11:01:19 -0600

    Update input.py
    Reflect on new unit testing.

--- examples/input.py
+++ examples/input.py
@@ -2,7 +2,7 @@
 import random
 from datetime import timedelta
 
-# Basic Math
+# Basic Math Testing
 x = 356
 y = 356
 
@@ -10,11 +10,16 @@ print(x + y)
 print(x / y)
 print(x * y)
 
-print('Hello World!')
-
 # Function Testing
 def test() -> int:
     return random.randint(1,100)
 
 print("The return", test())
 print(f"The F return {test()}")
+
+# Wrap Testing
+wrapper = lambda inter: inter() if callable(inter) == inter else inter
+
+print(
+    wrapper(wrapper(wrapper(wrapper(wrapper(test)))))()
+)
